69d6c24682537051314a4cbac23eebdb39675d9c
NeoklisMimidis <niomimidis@gmail.com>
2023-07-05 14:47:30 +0300

    update build command to work with repository and add clean:build command in scripts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/apprepository/playalong3/',
+  build: {
+    sourcemap: true,
+  },
   assetsInclude: ['**/*.jams'], // handle .jams files as static assets
-  // base: '/playalong/',
 });
